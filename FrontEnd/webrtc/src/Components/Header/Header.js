f27a045e6cf5174473ae44922b29940f63e1a9e8
sang_meeeee <goodman1223@naver.com>
2021-04-09 06:37:57 +0900

    FrontEnd

---
+++ FrontEnd/webrtc/src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from'react'
+import './Header.css'
+
+class Header extends React.Component{
+    render(){
+        return(
+            <div className="Header">
+                webRTC
+            </div>
+        )
+    }
+}
+
+export default Header
---
+++ FrontEnd/webrtc/src/Container/index.js
@@ -0,0 +1,5 @@
+import LoginContainer from './LoginContainer/LoginContainer'
+
+export {
+    LoginContainer
+}
