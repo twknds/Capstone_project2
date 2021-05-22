fb30f5d04de6e2b6b442d9f898b0162d8c042596
sangmeeeee <goodman1223@naver.com>
2021-05-22 16:10:49 +0900

    2021-05-22

--- FrontEnd/webrtc/src/Components/Video/Video.js
+++ FrontEnd/webrtc/src/Components/Video/Video.js
@@ -50,6 +50,17 @@ class Video extends React.Component {
             sendMessage('SDP', rtcSessionDescriptionInit);
         }
 
+        const getConnectedDevices = (type, callback) => {
+            navigator.mediaDevices.enumerateDevices()
+                .then(devices => {
+                    const filtered = devices.filter(device => device.kind === type);
+                    callback(filtered);
+            })
+        }
+        
+        getConnectedDevices('videoinput', cameras => console.log('Cameras found', cameras));
+
+
         navigator.mediaDevices
             .getUserMedia({ video: true, audio: false })
             .then(mediaStream => {
