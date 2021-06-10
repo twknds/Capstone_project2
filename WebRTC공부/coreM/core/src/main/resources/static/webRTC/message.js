c5b322a80fd8dd431289a2b6495547d5317aaf6e
sangmeeeee <goodman1223@naver.com>
2021-06-10 16:39:11 +0900

    delete folder

--- WebRTC공부/coreM/core/src/main/resources/static/webRTC/message.js
+++
@@ -1,5 +0,0 @@
-const socket = window.io();
-var socket = new WebSocket('ws://localhost:8080/socket');
-
-export const sendMessage = (type, payload) => socket.emit('message', {type, payload})
-export const onMessage = (type, callback) => socket.on('message', message => message.type === type && callback(message.payload))
